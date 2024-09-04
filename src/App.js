689b5ac1b894c3a51937f795ef875808132d8dc0
= <=>
2024-09-04 09:28:26 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactWhatsapp from "react-whatsapp";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<header className="App-header">*/}
+      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+      {/*  <p>*/}
+      {/*    Edit <code>src/App.js</code> and save to reload.*/}
+      {/*  </p>*/}
+      {/*  <a*/}
+      {/*    className="App-link"*/}
+      {/*    href="https://reactjs.org"*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    Learn React*/}
+      {/*  </a>*/}
+      {/*</header>*/}
+
+      <ReactWhatsapp number="+94 783233760" message="Hi." style={{backgroundColor:"#25D366", padding:"10px"}}>Whatsapp Inquiry</ReactWhatsapp>
+
     </div>
   );
 }
